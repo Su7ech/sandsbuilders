704eae803486f48f24929774e57862eab85f3216
su7ech <tpbaroody@gmail.com>
2016-04-29 14:57:33 -0700

    made changes to gallery javascript

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -9,9 +9,18 @@ $(function () {
 function getImages() {
   'use strict';
   $.getJSON('/assets/js/images.json', function (data) {
-    $.each(data.images.Bathrooms, function (key, val) {
-//      console.log(val.caption);
-      $('.gallery').append("<div class='col-xs-6 col-md-3'><a href='" + val.url + "' class='thumbnail'><img src='" + val.url + "' class='image img-responsive' alt='" + val.caption + "'></a><div>");
+    $.each(data.images.bathrooms, function (key, val) {
+      showImages(val.url, val.caption);
+    });
+    $.each(data.images.specialty, function (key, val) {
+      showImages(val.url, val.caption);
     });
   });
+}
+
+function showImages(url, caption) {
+  'use strict';
+  var imgContainer = $('.gallery');
+
+  imgContainer.append("<div class='col-xs-6 col-md-3'><a href='" + url + "' class='thumbnail'><img src='" + url + "' class='image img-responsive' alt='" + caption + "'></a><div>");
 }
