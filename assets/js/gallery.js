2fbbceaca860a7c11f856b9a94a04bee7eda9945
su7ech <tpbaroody@gmail.com>
2016-06-22 11:51:18 -0700

    Modified gallery javascript file

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,88 +1,49 @@
-/*jslint browser: true*/
-/*global $, jQuery, alert, console, getAlbums, displayAlbums, displayGallery, blueimp*/
+;(function() {
+  var Viewer = {
+    init: function() {
+      Viewer.getData();
+    },
+    getData: function() {
+      $.getJSON('/assets/js/images.json', function(data) {
+        $.each(data.albums, function(i, item) {
+          var name   = item.name,
+              photos = item.photos,
+              id     = item.id;
 
-$(function () {
-  'use strict';
-  getAlbums();
+          Viewer.displayAlbums(name, photos, id);
+        });
+      });
+    },
+    displayAlbums: function(name, photos, id) {
+      var album     = $('<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 thumb"><a class="thumbnail" href="javascript:;" title=""><img class="image" src="" alt="" /><div class="caption"><h4></h4></div></a></div>').clone(true),
+          thumbnail = album.find('.thumbnail'),
+          image     = album.find('.thumbnail .image'),
+          caption   = album.find('.thumbnail .caption h4');
 
-});
+      image.attr('src', photos[0].href).attr('alt', name);
+      caption.html(name);
+      
+      $('.albums').append(album);
 
-function getAlbums() {
-  'use strict';
-  $.getJSON('/assets/js/images.json', function (data) {
-    $.each(data.albums, function (i, item) {
-      var name   = item.name,
-          photos = item.photos,
-          id     = item.id;
+      album.on('click', 'a', function() {
+        $('.content').empty().removeClass('albums').addClass('gallery').show();
+        $.each(photos, function(i, item) {
+          var photo = item.href;
+          Viewer.displayGallery(photo, name); 
+        });
+      });
+    },
+    displayGallery: function(photo, name) {
+      var gallery = $('<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 thumb"><a class="thumbnail" href="" title="" data-gallery><img class="image" src="" alt="" /></a></div>').clone(),
+          link    = gallery.find('.thumbnail'),
+          image   = gallery.find('.thumbnail img');
 
-      displayAlbums(name, photos, id);
-    });
-  });
-}
+      link.attr('href', photo).attr('title', name);
+      image.attr('src', photo).attr('alt', name);
 
-function updateHistory(data) {
-  var dataToSave = {
-    title: data.title,
-    href: data.href,
-    url: data.dataset.url
-  }
-
-  history.pushState(
-    dataToSave,
-    data.title,
-    data.dataset.url
-  );
-}
-
-function handleState() {
-  $(window).on('popstate', function(e) {
-    if (!e.originalEvent.state) {
-      $('.img-container .gallery').hide();
-      $('.img-container .albums').show();
+      $('.gallery').append(gallery);
     }
-    console.log(e);
-  });
-}
+  };
 
-function displayAlbums(name, photos, id) {
-  'use strict';
-  var albumTemplate    = $('<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 thumb"><a class="thumbnail" href="javascript:;" title="" data-url=""><img class="image" src="" alt="" /><div class="caption"><h4></h4></div></a></div>'),
-      album       = albumTemplate.clone(true),
-      thumbnail   = album.find('.thumbnail'),
-      image       = album.find('.thumbnail .image'),
-      caption     = album.find('.thumbnail .caption h4');
-
-  caption.html(name);
-  thumbnail.attr('title', name);
-  thumbnail.attr('data-url', name);
-  image.attr('src', photos[0].href);
-  image.attr('alt', photos[0].href);
-
-  album.on('click', 'a', function() {
-    $('.img-container .albums').hide();
-    $('.img-container .gallery').empty().show();
-    updateHistory(this);
-    handleState();
-    $.each(photos, function(i, item) {
-      var photo = item.href;
-      displayGallery(photo, name);
-    });
-  });  
-  $('.img-container .albums').append(album);
-}
-
-function displayGallery(photo, name) {
-  'use strict';
-  var galleryTemplate = $('<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 thumb"><a class="thumbnail" href="" title="" data-gallery><img class="image" src="" alt="" /></a></div>'),
-      gallery         = galleryTemplate.clone(),
-      link            = gallery.find('.thumbnail'),
-      image           = gallery.find('.thumbnail img');
-
-  link.attr('href', photo);
-  link.attr('title', name);
-
-  image.attr('src', photo);
-  image.attr('alt', name);
-
-  $('.img-container .gallery').append(gallery);
-}
+  Viewer.init();
+})();
